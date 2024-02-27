4c619655cb267a366819b0760476a3cdf471d365
Sergi <sergimima@gmail.com>
2024-02-27 17:10:26 +0100

    NewChainUpdate  - Inex bootstrap update - main.jsx añadida ruta para probar newchain temporalmente

--- front/src/components/NewChain.jsx
+++ front/src/components/NewChain.jsx
@@ -1,22 +1,59 @@
-//Formulario para crear la red que nos pida: NUMERO DE RED. Creará la red con el nodo1
+/////////Formulario para crear la red que nos pida: NUMERO DE RED. Creará la red con el nodo1
 
-import React from "react";
+//El formulario tendrá que tener un input tipo numero para el ChainID
+//Tendra un boton donde llame la funcion onSubmit para llamar la funcion al backend de añadir un nodo
+//Antes pero se tendrà que verificar que ese ChainID no esté ya registrado, y si lo está, que lance un error avisando
+
+import React, { useState }  from "react";
 import {useForm } from "react-hook-form";
 
 
 export function NewChain(){
+    //Declaramos el handleSubmit para recojer los datos del formulario
+    const { register, handleSubmit } = useForm();
 
-    const onSubmit = async (data) =>{
+    //Declaramos un state error, para lanzar error en caso de que no exista, lo inizializamos como null
+    const [error, setError] = useState(null);
 
+    //Creamos la funcion para añadir la red, será asincrona
+    async function createChain(){
+        const url = `RUTA PARA LA FUNCION DE AÑADIR LA RED ${chainID}`
+        const response = await fetch(url)
     }
 
-    return <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="form-group">
-                        <label htmlFor="">Introduzca el número de red "CainID"</label>
-                        <input {...register('chainID')} type="number" className="form-control"></input>
-                    </div>
-                    <button className="btn btn-primary mt-3">Añadir red</button>
+    const onSubmit = async (data) =>{
+        
+        const chainID = data.chainID;
+
+        //Comprobamos que el data llega desde el formulario
+        console.log(chainID)
+
+        const exists = await fetch(`RUTA PARA LA FUNCIÓN DE COMPROBAR CHAIN DADO UN ID${chainID}`)
+        const result = await exists.json();
+
+        //Comprobamos el resultado
+        console.log(result)
+
+        //Si el resultado da 0, no existe, sino, ya esta en uso
+        if(result.lenght === 0){
+            //Ejecutamos la funcion para añadir la red, con el parametro chainID
+            createChain(chainID)
+
+        } else {
+            //Avisamos por consola que no llega
+            console.log("Doesn´t exist");
+            setError("Este chain ID no está disponible")
+        }
+    }
+    return <div className="container mt-4">
+        
+        {error && <h1 className="alert">{error}</h1>}
+        <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+                <label className="form-style" htmlFor="">Introduzca el número de red "ChainID"</label>
+                <input {...register('chainID')} type="number" className="form-control"></input>
+            </div>
+            <button className="btn btn-primary mt-3">Añadir red</button>
         </form>
     </div>
     
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
+import { NewChain } from './components/NewChain.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-
+//Ruta rapida creada para yo poder ver el NewChain
+function App(){
+  return <BrowserRouter >
+          <Routes>
+            <Route path='/' element={<NewChain></NewChain>}>
+              <Route path='/newchain' element={<NewChain></NewChain>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter >
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
