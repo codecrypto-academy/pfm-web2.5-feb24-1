78298d2aae47217500af01ee728c4f14ff7c4120
Presen <cryptospacespain@gmail.com>
2024-02-28 17:01:43 +0100

    Presen:
    - Subida de la pagina inicial más pulida, con una cabecerá con los enrutados a distintos componentes.
    - Se incorpora footer.
    - Se actualiza README para incorporar versiones.

---
+++ front/src/components/Nodos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Nodos() {
+    return <div>
+        <h1>Estás en Nodos</h1>
+    </div>
+}
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
-import { NewChain } from './components/NewChain.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+//import App from './App.jsx'
+import { NewChain } from './components/NewChain.jsx'
+import { Inicio } from './components/Inicio.jsx'
+import { Perfil } from './components/Perfil.jsx'
+import { Redes } from './components/Redes.jsx'
+import { Nodos } from './components/Nodos.jsx'
+import './index.css'
+
+
 //Ruta rapida creada para yo poder ver el NewChain
-function App(){
-  return <BrowserRouter >
+function App() {
+  /*return <BrowserRouter >
           <Routes>
             <Route path='/' element={<NewChain></NewChain>}>
               <Route path='/newchain' element={<NewChain></NewChain>}/>
             </Route>
           </Routes>
-        </BrowserRouter >
+        </BrowserRouter >*/
+  return <BrowserRouter >
+    <Routes>
+      <Route path='/' element={<Inicio></Inicio>}>
+        <Route index element={<NewChain></NewChain>}></Route>
+        <Route path='*' element={<NewChain></NewChain>}></Route>
+        <Route path='newchain' element={<NewChain></NewChain>}></Route>
+        <Route path='perfil' element={<Perfil></Perfil>}></Route>
+        <Route path='redes' element={<Redes></Redes>}></Route>
+        <Route path='nodos' element={<Nodos></Nodos>}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter >
 }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
