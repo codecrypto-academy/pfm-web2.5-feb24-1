4d0c309ddcc4d2e198141dda67b1cb03a8b87cfe
Presen <cryptospacespain@gmail.com>
2024-02-28 17:29:48 +0100

    Presen:
    - Se crean componentes Privacidad, QuienesSomos y TerminosCondiciones. Se inncluyen los enrutados.
    - Se comenta mucho más "main.jsx", la parte de footer.

---
+++ front/src/components/Privacidad.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Privacidad() {
+    return <div>
+        <h1>Estás en Privacidad</h1>
+    </div>
+}
---
+++ front/src/components/QuienesSomos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function QuienesSomos() {
+    return <div>
+        <h1>Estás en Quienes Somos</h1>
+    </div>
+}
